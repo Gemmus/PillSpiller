2b4b9de5e0bebf456d949cc6807ae46c18169251
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-12-20 10:50:08 +0200

    Update lorawan_dummy_main.cpp

--- lorawan_dummy_main.cpp
+++ lorawan_dummy_main.cpp
@@ -23,7 +23,9 @@ int main(void) {
 
     stdio_init_all();
     buttonInit();
-    lora_connected = loraInit();
+    while (!lora_connected) {
+        lora_connected = loraInit();
+    }
 
     char retval_str[STRLEN];
     char dummy_msg1[STRLEN];
@@ -31,6 +33,7 @@ int main(void) {
     char dummy_msg2[STRLEN];
     strcpy(dummy_msg2, "I am messaging, yay!");
 #if 0
+    /* to set the uart TIMEOUT value: */
     if (true == loraCommunication("AT+UART=TIMEOUT,300\r\n", STD_WAITING_TIME, retval_str)) {
         printf("%s\n",retval_str);
     }
@@ -52,7 +55,9 @@ int main(void) {
         if (true == lora_send && true == lora_connected) {
             if (loraMsg(dummy_msg1, strlen(dummy_msg1), retval_str)) {
                 printf("%s\n", retval_str);
-            } 
+            } else {
+                printf("Message not sent successfully.\n");
+            }
             lora_send = false;
         }
     }
