11e7e281f38dcd2c7b26f62c85e7cd2d373e8cb1
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-12-20 20:14:38 +0200

    Update steppermotor.h

--- steppermotor.h
+++ steppermotor.h
@@ -6,11 +6,17 @@
 #define IN2 6
 #define IN3 3
 #define IN4 2
-#define ALIGNMENT 424
+#define ALIGNMENT 380
 #define COMPARTMENTS 8
+#define SLEEPTIME_BETWEEN 30000
 
 /*  OPTOFORK  */
 #define OPTOFORK 28
+
+/*  PIEZO  */
+#define PILL_DROP_MS 100
+#define BLINK_TIMES 5
+#define PIEZO 27
 
 void stepperMotorInit();
 void calibrateMotor();
@@ -18,5 +24,6 @@ void runMotorAntiClockwise(int times);
 void runMotorClockwise(int times);
 void optoforkInit();
 void optoFallingEdge();
+void piezoInit();
 
 #endif
