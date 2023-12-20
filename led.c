6169a15a529abd4300fd40aed7dae9a33f9916f3
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-12-20 20:12:26 +0200

    blink() added

--- led.c
+++ led.c
@@ -1,4 +1,5 @@
 #include "led.h"
+#include "pico/time.h"
 #include "hardware/gpio.h"
 #include "hardware/pwm.h"
 
@@ -14,14 +15,14 @@ void ledsInit() {
 
 void pwmInit() {
     pwm_config config = pwm_get_default_config();
+    pwm_config_set_clkdiv_int(&config, DIVIDER);
+    pwm_config_set_wrap(&config, PWM_FREQ - 1);
 
     // D1: 2A, D2: 2B, D3: 3A
     for (int i = 0; i < sizeof(led_arr)/ sizeof(led_arr[0]); i++) {
         uint dX_slice = pwm_gpio_to_slice_num(led_arr[i]);
         uint dX_chanel = pwm_gpio_to_channel(led_arr[i]);
         pwm_set_enabled(dX_slice, false);
-        pwm_config_set_clkdiv_int(&config, DIVIDER);
-        pwm_config_set_wrap(&config, PWM_FREQ - 1);
         pwm_init(dX_slice, &config, false);
         pwm_set_chan_level(dX_slice, dX_chanel, LEVEL + 1);
         gpio_set_function(led_arr[i], GPIO_FUNC_PWM);
@@ -40,4 +41,11 @@ void allLedsOff() {
     for (int i = 0; i < sizeof(led_arr)/ sizeof(led_arr[0]); i++) {
         pwm_set_gpio_level(led_arr[i], MIN_BRIGHTNESS);
     }
+}
+
+void blink() {
+    allLedsOn();
+    sleep_ms(BLINK_SLEEP_TIME);
+    allLedsOff();
+    sleep_ms(BLINK_SLEEP_TIME);
 }
