693752b03fb635d26203685665eb7052c498312b
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-12-21 20:20:19 +0200

    Update steppermotor.c

--- steppermotor.c
+++ steppermotor.c
@@ -2,6 +2,13 @@
 #include "steppermotor.h"
 #include <stdio.h>
 
+#ifndef DEBUG_PRINT
+#define DBG_PRINT(f_, ...)  printf((f_), ##__VA_ARGS__)
+#else
+#define DBG_PRINT(f_, ...)
+#endif
+
+static const int stepper_array[] = {IN1, IN2, IN3, IN4};
 static const uint turning_sequence[8][4] = {{1, 0, 0, 0},
                                             {1, 1, 0, 0},
                                             {0, 1, 0, 0},
@@ -11,12 +18,11 @@ static const uint turning_sequence[8][4] = {{1, 0, 0, 0},
                                             {0, 0, 0, 1},
                                             {1, 0, 0, 1}};
 
+static volatile int row = 0;
+volatile int revolution_counter = 0;
+volatile int calibration_count = 0;
 volatile bool calibrated = false;
 volatile bool fallingEdge = false;
-static volatile int revolution_counter = 0;
-volatile int calibration_count = 0;
-static volatile int row = 0;
-static const int stepper_array[] = {IN1, IN2, IN3, IN4};
 volatile bool pill_detected = false;
 
 void stepperMotorInit() {
@@ -30,16 +36,14 @@ void calibrateMotor() {
     calibrated = false;
     fallingEdge = false;
     while (false == fallingEdge) {
-        //runMotorAntiClockwise(1);
         runMotorClockwise(1);
     }
     fallingEdge = false;
     while (false == fallingEdge) {
-        //runMotorAntiClockwise(1);
         runMotorClockwise(1);
     }
     calibrated = true;
-    printf("Number of steps per revolution: %u\n", calibration_count);
+    DBG_PRINT("Number of steps per revolution: %u\n", calibration_count);
     runMotorAntiClockwise(ALIGNMENT);
 }
 
@@ -57,10 +61,9 @@ void runMotorAntiClockwise(int times) {
 
 void runMotorClockwise(int times) {
     for(; times > 0; times--) {
-        gpio_put(IN4, turning_sequence[row][3]);
-        gpio_put(IN3, turning_sequence[row][2]);
-        gpio_put(IN2, turning_sequence[row][1]);
-        gpio_put(IN1, turning_sequence[row][0]);
+        for (int j = 0; j < sizeof(stepper_array) / sizeof(stepper_array[0]); j++) {
+            gpio_put(stepper_array[j], turning_sequence[row][j]);
+        }
         revolution_counter++;
         if (--row <= -1) {
             row = 7;
@@ -87,4 +90,16 @@ void piezoInit() {
     gpio_init(PIEZO);
     gpio_set_dir(PIEZO, GPIO_IN);
     gpio_pull_up(PIEZO);
+}
+
+void piezoFallingEdge() {
+    pill_detected = true;
+}
+
+void gpioFallingEdge(uint gpio, uint32_t event_mask) {
+    if (OPTOFORK == gpio) {
+        optoFallingEdge();
+    } else {
+        piezoFallingEdge();
+    }
 }
