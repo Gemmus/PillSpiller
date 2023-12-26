f32b61dcdd0e5bad155be5b6547446d182c1351b
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-12-26 10:06:57 +0200

    Added memory functionality. Eeprom msg log and lorawan not implemented.

--- main.c
+++ main.c
@@ -19,22 +19,11 @@
 #endif
 
 /////////////////////////////////////////////////////
-//                      MACROS                     //
-/////////////////////////////////////////////////////
-
-
-/////////////////////////////////////
-//      MEMORY ADDRESSES           //
-/////////////////////////////////////
-const uint16_t deviceStateAddr = I2C_MEM_SIZE - 1;  // 1 byte: 0 or 1
-const uint16_t compartmentFinishedAddr = I2C_MEM_SIZE - 2; // 1 byte: 0 or 1
-const uint16_t calibCountAddr = MEM_ADDR_START; // 4 bytes
-const uint16_t compartmentsMovedAddr = MAX_LOG_SIZE; // 4 bytes
-
-/////////////////////////////////////////////////////
 //             FUNCTION DECLARATIONS               //
 /////////////////////////////////////////////////////
 bool repeatingTimerCallback(struct repeating_timer *t);
+void resetValues();
+void dispensePills();
 
 /////////////////////////////////////////////////////
 //                GLOBAL VARIABLES                 //
@@ -43,7 +32,6 @@ static volatile bool sw0_buttonEvent = false;
 static volatile bool sw2_buttonEvent = false;
 
 extern int calibration_count;
-extern int revolution_counter;
 extern bool calibrated;
 extern bool pill_detected;
 extern bool fallingEdge;
@@ -51,19 +39,12 @@ extern bool fallingEdge;
 /////////////////////////////////////////////////////
 //                 ENUM for STATES                 //
 /////////////////////////////////////////////////////
-enum SystemState {
-    CALIB_WAITING,       // EEPROM, CALIBRATED: 0 == CALIB_WAITING
-    DISPENSE_WAITING     //                     1 == DISPENSE_WAITING
+machineState machine = {
+        .currentState = CALIB_WAITING,
+        .compartmentFinished = IN_THE_MIDDLE,
+        .calibrationCount = 0,
+        .compartmentsMoved = 0,
 };
-
-enum SystemState currentState = CALIB_WAITING;
-
-enum CompartmentState {
-    IN_THE_MIDDLE,      // EEPROM, 0 == IN_THE_MIDDLE
-    FINISHED            //         1 == FINISHED
-};
-
-enum CompartmentState compartmentFinished = IN_THE_MIDDLE;
 
 /////////////////////////////////////////////////////
 //                     MAIN                        //
@@ -86,79 +67,28 @@ int main(void) {
     gpio_set_irq_enabled_with_callback(OPTOFORK, GPIO_IRQ_EDGE_FALL, true, gpioFallingEdge);
     gpio_set_irq_enabled(PIEZO, GPIO_IRQ_EDGE_FALL, true);
 
-    int compartmentsMoved = 1;
+    DBG_PRINT("Boot\n"); /* MSG */
 
-    // check if it was powered off in the middle of motor turning
-    bool stored_state = i2cReadByte(deviceStateAddr);
-    DBG_PRINT("%d", stored_state);
-    if (1 == stored_state) {
-        calibrated = true;
-        allLedsOff();
-        currentState = DISPENSE_WAITING;
+    if (readStruct(&machine)) {
+        if (machine.currentState == DISPENSE_WAITING) {
+            calibration_count = machine.calibrationCount;
+            calibrated = true;
+            allLedsOff();
 
-        calibration_count = readInt(calibCountAddr);
-        DBG_PRINT("Calibration count stored %d\n", calibration_count);
-        if (0 < calibration_count) {
-            compartmentsMoved = readInt(compartmentsMovedAddr);
-            DBG_PRINT("Compartments moved: %d\n", compartmentsMoved);
-            if (0 < compartmentsMoved) {
-                compartmentFinished = i2cReadByte(compartmentFinishedAddr);
-                DBG_PRINT("CompartmentFinished %s", compartmentFinished);
-                switch (compartmentFinished) {
-                    case IN_THE_MIDDLE:
-                        while (false == fallingEdge) {
-                            runMotorAntiClockwise(1);
-                        }
-                        fallingEdge = false;
-                        for (int i = 0; i < ALIGNMENT; i++) {
-                            runMotorClockwise(1);
-                        }
-                        for (int i = 0; i < compartmentsMoved; i++) {
-                            for (int j = 0; j < (calibration_count / COMPARTMENTS + COMPARTMENTS - 1); j++) {
-                                runMotorClockwise(1);
-                                if (j == 0) {
-                                    compartmentFinished = IN_THE_MIDDLE;
-                                    i2cWriteByte(compartmentFinishedAddr, compartmentFinished);
-                                }
-                            }
-                            compartmentFinished = FINISHED;
-                            i2cWriteByte(compartmentFinishedAddr, compartmentFinished);
-                        }
-                        sleep_ms(SLEEP_BETWEEN);
-                    case FINISHED:
-                        for (; compartmentsMoved <= (COMPARTMENTS - 1); compartmentsMoved++) {
-                            for (int i = 0; i < (calibration_count / COMPARTMENTS + COMPARTMENTS - 1); i++) {
-                                runMotorClockwise(1);
-                                if (i == 0) {
-                                    compartmentFinished = IN_THE_MIDDLE;
-                                    i2cWriteByte(compartmentFinishedAddr, compartmentFinished);
-                                }
-                            }
-                            if (true == pill_detected) {
-                                pill_detected = false;
-                                for (int i = 0; i < BLINK_TIMES; i++) {
-                                    blink();
-                                }
-                            }
-                            compartmentFinished = FINISHED;
-                            i2cWriteByte(compartmentFinishedAddr, compartmentFinished);
-                            writeInt(compartmentsMovedAddr, compartmentsMoved);
-                            if ((COMPARTMENTS - 1) != compartmentsMoved ) {
-                                sleep_ms(SLEEP_BETWEEN / 4);
-                            } else {
-                                DBG_PRINT("All pills dispensed. Waiting for button press to calibrate.\n");
-                            }
-                        }
-                        currentState = CALIB_WAITING;
-                        i2cWriteByte(deviceStateAddr, currentState);
-                        compartmentFinished = IN_THE_MIDDLE;
-                        i2cWriteByte(compartmentFinishedAddr, compartmentFinished);
-                        calibration_count = 0;
-                        writeInt(calibCountAddr, calibration_count);
-                        compartmentsMoved = 0;
-                        writeInt(compartmentsMovedAddr, compartmentsMoved);
-                        break;
-                }
+            switch (machine.compartmentFinished) {
+                case IN_THE_MIDDLE:
+                    realignMotor();
+                    sleep_ms(SLEEP_BETWEEN / 8);
+                    machine.compartmentFinished = FINISHED;
+                    writeStruct(&machine);
+                    dispensePills();
+                    break;
+                case FINISHED:
+                    machine.compartmentsMoved++;
+                    sleep_ms(SLEEP_BETWEEN / 8);
+                    dispensePills();
+                    resetValues();
+                    break;
             }
         }
     }
@@ -166,13 +96,15 @@ int main(void) {
     while(true) {
         if (true == sw0_buttonEvent) {
             sw0_buttonEvent = false;
-            switch (currentState) {
+            switch (machine.currentState) {
                 case CALIB_WAITING:
                     calibrateMotor(); /* calibrates and aligns */
                     allLedsOn();
-                    currentState = DISPENSE_WAITING;
-                    i2cWriteByte(deviceStateAddr, currentState);
-                    writeInt(calibCountAddr, calibration_count);
+                    machine.currentState = DISPENSE_WAITING;
+                    machine.calibrationCount = calibration_count;
+                    writeStruct(&machine);
+                    machine.compartmentsMoved = 1;
+                    //DBG_PRINT("Sent currentStat %d\n", currentState);
                     break;
                 case DISPENSE_WAITING:
                     break;
@@ -181,49 +113,17 @@ int main(void) {
 
         if (true == sw2_buttonEvent) {
             sw2_buttonEvent = false;
-            switch (currentState) {
+            switch (machine.currentState) {
                 case CALIB_WAITING:
                     break;
                 case DISPENSE_WAITING:
-                    allLedsOff();
-                    pill_detected = false;
-                    /* start dispensing pills */
-                    for (; compartmentsMoved <= (COMPARTMENTS - 1); compartmentsMoved++) {
-                        for (int i = 0; i < (calibration_count / COMPARTMENTS + COMPARTMENTS - 1); i++) {
-                            runMotorClockwise(1);
-                            if (i == 0) {
-                                compartmentFinished = IN_THE_MIDDLE;
-                                i2cWriteByte(compartmentFinishedAddr, compartmentFinished);
-                            }
-                        }
-                        if (true == pill_detected) {
-                            pill_detected = false;
-                            for (int i = 0; i < BLINK_TIMES; i++) {
-                                blink();
-                            }
-                        }
-                        compartmentFinished = FINISHED;
-                        i2cWriteByte(compartmentFinishedAddr, compartmentFinished);
-                        writeInt(compartmentsMovedAddr, compartmentsMoved);
-                        if ((COMPARTMENTS - 1) != compartmentsMoved ) {
-                            sleep_ms(SLEEP_BETWEEN / 4);
-                        } else {
-                            DBG_PRINT("All pills dispensed. Waiting for button press to calibrate.\n");
-                        }
-                    }
-                    currentState = CALIB_WAITING;
-                    i2cWriteByte(deviceStateAddr, currentState);
-                    compartmentFinished = IN_THE_MIDDLE;
-                    i2cWriteByte(compartmentFinishedAddr, compartmentFinished);
-                    calibration_count = 0;
-                    writeInt(calibCountAddr, calibration_count);
-                    compartmentsMoved = 0;
-                    writeInt(compartmentsMovedAddr, compartmentsMoved);
+                    dispensePills();
+                    resetValues();
                     break;
             }
         }
 
-        if (CALIB_WAITING == currentState) {
+        if (CALIB_WAITING == machine.currentState) {
             blink();
         }
     }
@@ -262,4 +162,45 @@ bool repeatingTimerCallback(struct repeating_timer *t) {
         sw2_filter_counter = 0;
     }
     return true;
+}
+
+void dispensePills() {
+    allLedsOff();
+    pill_detected = false;
+    /* start dispensing pills */
+    for (; machine.compartmentsMoved < COMPARTMENTS; machine.compartmentsMoved++) {
+        DBG_PRINT("Compartments moved %d\n", machine.compartmentsMoved);
+        for (int i = 0; i < (calibration_count / COMPARTMENTS + COMPARTMENTS - 1); i++) {
+            runMotorClockwise(1);
+            if (i == 0) {
+                machine.compartmentFinished = IN_THE_MIDDLE;
+                writeStruct(&machine);
+            }
+            if (i % 4 == 0) {
+                i2cWriteByte(I2C_MEM_SIZE/2, i/4);
+            }
+            if (true == pill_detected) {
+                pill_detected = false;
+                for (int j = 0; j < BLINK_TIMES; j++) {
+                    blink();
+                }
+            }
+        }
+        machine.compartmentFinished = FINISHED;
+        writeStruct(&machine);
+        DBG_PRINT("Before comparison: Compartments moved %d\n", machine.compartmentsMoved);
+        if ((COMPARTMENTS - 1) > machine.compartmentsMoved) {
+            sleep_ms(SLEEP_BETWEEN / 8);
+        } else {
+            DBG_PRINT("All pills dispensed. Waiting for button press to calibrate.\n");
+        }
+    }
+}
+
+void resetValues() {
+    machine.currentState = CALIB_WAITING;
+    machine.compartmentFinished = IN_THE_MIDDLE;
+    machine.calibrationCount = 0;
+    machine.compartmentsMoved = 0;
+    writeStruct(&machine);
 }
