21a24ac0d91149502aa88caeb511a1c52b3ae590
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-12-27 17:36:41 +0200

    Update button.c

--- button.c
+++ button.c
@@ -1,8 +1,25 @@
 #include "button.h"
 #include "hardware/gpio.h"
 
-static const int button_arr[] = {SW_0, SW_1, SW_2};
+//////////////////////////////////////////////////
+//              GLOBAL VARIABLES                //
+//////////////////////////////////////////////////
 
+static const int button_arr[] = {SW_0, SW_2};
+
+//////////////////////////////////////////////////
+//               BUTTON FUNCTIONS               //
+//////////////////////////////////////////////////
+
+/**********************************************************************************************************************
+ * \brief: Initializes the SW_0 and SW_2 buttons
+ *
+ * \param:
+ *
+ * \return:
+ *
+ * \remarks:
+ **********************************************************************************************************************/
 void buttonsInit() {
     for (int i = 0; i < sizeof(button_arr)/sizeof(button_arr[0]); i++) {
         gpio_init(button_arr[i]);
