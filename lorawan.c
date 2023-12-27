5f81666a70ff50419b14e7379e87f9245ca49556
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-12-27 17:32:46 +0200

    Update lorawan.c

--- lorawan.c
+++ lorawan.c
@@ -12,8 +12,11 @@
 #define DBG_PRINT(f_, ...)
 #endif
 
+//////////////////////////////////////////////////
+//              GLOBAL VARIABLES                //
+//////////////////////////////////////////////////
+
 static const int uart_nr = UART_NR;
-static volatile int lorawanState = 0;
 static lorawan_item lorawan[] = {{"AT\r\n", "+AT: OK\r\n", STD_WAITING_TIME},
                                  {"AT+MODE=LWOTAA\r\n", "+MODE: LWOTAA\r\n", STD_WAITING_TIME},
                                  {"AT+KEY=APPKEY,\"511F30D4D81E7B806536733DE7155FDE\"\r\n", "+KEY: APPKEY 511F30D4D81E7B806536733DE7155FDE\r\n", STD_WAITING_TIME},  // Gemma
@@ -23,47 +26,55 @@ static lorawan_item lorawan[] = {{"AT\r\n", "+AT: OK\r\n", STD_WAITING_TIME},
                                  {"AT+PORT=8\r\n", "+PORT: 8\r\n", STD_WAITING_TIME},
                                  {"AT+JOIN\r\n", "Network joined\r\n", MSG_WAITING_TIME}};
 
+//////////////////////////////////////////////////
+//              LORAWAN FUNCTIONS               //
+//////////////////////////////////////////////////
+
+/**********************************************************************************************************************
+ * \brief: Initialises the uart and sets up lorawan communication
+ *
+ * \param:
+ *
+ * \return: true: if connection established, false: if connection not established
+ *
+ * \remarks: Programmer should use this to initialize uart and lorawan communication.
+ **********************************************************************************************************************/
 bool loraInit() {
-    uint count = 0;
     char return_message[STRLEN];
+    int lorawanState = 0;
 
     uart_setup(UART_NR, UART_TX_PIN, UART_RX_PIN, BAUD_RATE);
 
     while (true) {
-        if (0 == lorawanState) {
-            while (MAX_COUNT > count++) {
-                if(true == retvalChecker(lorawanState)) {
-                    lorawanState++;
-                }
-            }
-            if (MAX_COUNT == count) {
-                DBG_PRINT("Attempted to connect 5 times, Lorawan module not responding.\n");
-                return false;
-            }
-        }
-        for (lorawanState; lorawanState < (sizeof(lorawan)/sizeof(lorawan[0]) - 1); lorawanState++) {
+        for (; lorawanState < (sizeof(lorawan)/sizeof(lorawan[0]) - 1); lorawanState++) {
             if (false == retvalChecker(lorawanState)) {
-                lorawanState = 0;
                 return false;
             }
         }
         if (true == loraCommunication(lorawan[lorawanState].command, lorawan[lorawanState].sleep_time, return_message)) {
             if (strstr(return_message, lorawan[lorawanState].retval) != NULL) {
                 DBG_PRINT("Comparison->same for: %s\n", return_message);
-                lorawanState = 0;
                 return true;
             }
         }
-        lorawanState = 0;
         return false;
     }
 }
 
+/**********************************************************************************************************************
+ * \brief: Communicates with uart.
+ *
+ * \param: 3 parameters. Takes the command to be sent, the sleep time to wait before read and the string to read the
+ *         returned message to.
+ *
+ * \return: true: if uart responses, false: if uart does not response
+ *
+ * \remarks: Called by loraInit(), loraMsg(), retvalChecker(). Can be used directly from main().
+ **********************************************************************************************************************/
 bool loraCommunication(const char* command, const uint sleep_time, char* str) {
-    int pos = 0;
     uart_send(uart_nr, command);
     sleep_ms(sleep_time);
-    pos = uart_read(UART_NR, (uint8_t *) str, STRLEN - 1);
+    int pos = uart_read(UART_NR, (uint8_t *) str, STRLEN - 1);
     if (pos > 0) {
         str[pos] = '\0';
         return true;
@@ -71,6 +82,15 @@ bool loraCommunication(const char* command, const uint sleep_time, char* str) {
     return false;
 }
 
+/**********************************************************************************************************************
+ * \brief: Formats message and sends it to uart.
+ *
+ * \param: 3 parameters. Takes the message to be sent, message length and string where the returned message is read to.
+ *
+ * \return: true: if uart responses, false: if uart does not response
+ *
+ * \remarks: Programmer should use this to send message.
+ **********************************************************************************************************************/
 bool loraMsg(const char* message, size_t msg_size, char* return_message) {
 
     const char start_tag[] = "AT+MSG=\"";
@@ -93,12 +113,21 @@ bool loraMsg(const char* message, size_t msg_size, char* return_message) {
     }
 }
 
+/**********************************************************************************************************************
+ * \brief: Calls loraCommunication and compares the returned value with the value in case of success.
+ *
+ * \param: 1 parameter. Takes the index of the struct element.
+ *
+ * \return: true: if compared strings are the same, false: if compared strings are not the same
+ *
+ * \remarks: Called by loraInit(). Programmer should not use this function.
+ **********************************************************************************************************************/
 bool retvalChecker(const int index) {
     char return_message[STRLEN];
 
     if (true == loraCommunication(lorawan[index].command, lorawan[index].sleep_time, return_message)) {
         if (strcmp(lorawan[index].retval, return_message) == 0) {
-            DBG_PRINT("Comparison->same for: %s\n", return_message);
+            //DBG_PRINT("Comparison->same for: %s\n", return_message);
             return true;
         } else {
             DBG_PRINT("Comparison->no match, return_message: %s lorawan[%d].retval: %s\n", return_message, index, lorawan[index].retval);
